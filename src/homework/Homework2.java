5cfc64587dd5490ee781cc1d9a565077aba8cfbb
dimmxx <dimmxx@gmail.com>
2018-12-03 00:03:40 +0200

    03.12.18

--- src/homework/Homework2.java
+++ src/homework/Homework2.java
@@ -52,7 +52,7 @@ public class Homework2 {
 
         do {
 
-            if (scan.hasNext("d") & pos < array.length) {
+            if (scan.hasNext("d")) {
                 array[pos] = '_';
                 if (pos != array.length - 1){
                     pos++;
@@ -60,7 +60,7 @@ public class Homework2 {
                 array[pos] = dog;
                 printRunDog(array);
                 scan.next();
-            } else if (scan.hasNext("a") & pos < array.length) {
+            } else if (scan.hasNext("a")) {
                 array[pos] = '_';
                 if (pos != 0){
                     pos--;
@@ -75,11 +75,12 @@ public class Homework2 {
             }
 
         } while (true);
+        scan.close();
 
     }
 
     public static void printRunDog(char[] array){
-        System.out.print("\r");
+        //System.out.print("\r");
         for(int i = 0; i < array.length; i++){
             System.out.print(array[i]);
         }
@@ -91,27 +92,67 @@ public class Homework2 {
         for (int i = 0; i < array.length; i++){
             for(int k = 0; k < array[i].length; k++) array[i][k] = '_';
         }
-        array[0][0] = '@';
-        printRunDogMatrix(array);
-        array[4][4] = 'r';
+
+        char dog = '@';
+        array[0][0] = dog;
+        int pos = 0;
+        int high = 0;
+
+        Scanner scan = new Scanner(System.in);
+
         printRunDogMatrix(array);
 
+        do {
+
+            if (scan.hasNext("d")) {
+                array[pos][high] = '_';
+                if (pos != array.length - 1){
+                    pos++;
+                } else break;
+                array[pos][high] = dog;
+                printRunDogMatrix(array);
+                scan.next();
+            } else if (scan.hasNext("a")) {
+                array[pos][high] = '_';
+                if (pos != 0){
+                    pos--;
+                } else break;
+                array[pos][high] = dog;
+                printRunDogMatrix(array);
+                scan.next();
+            } else if (scan.hasNext("s")){
+                array[pos][high] = '_';
+                if (high != array[pos].length - 1){
+                    high++;
+                } else break;
+                array[pos][high] = dog;
+                printRunDogMatrix(array);
+                scan.next();
+            } else if (scan.hasNext("w")){
+                array[pos][high] = '_';
+                if (high != 0){
+                    high--;
+                } else break;
+                array[pos][high] = dog;
+                printRunDogMatrix(array);
+                scan.next();
+            } else {
+                printRunDogMatrix(array);
+                System.out.println(" Press A, D, W or S!");
+                scan.next();
+            }
+
+        } while (true);
     }
 
     public static void printRunDogMatrix(char[][] array){
+        System.out.println();
         for(int i = 0; i < array.length; i++) {
             System.out.println();
             for (int k = 0; k < array[i].length; k++){
-                System.out.print(array[i][k]);
+                System.out.print(array[k][i]);
             }
         }
-
-
-
-
+        System.out.println();
     }
-
-
-
-
 }
--- src/lesson/Recursion.java
+++ src/lesson/Recursion.java
@@ -2,46 +2,47 @@ package lesson;
 
 public class Recursion {
 
+
+    static int res = 0;
+
     public static void main(String[] args) {
 
         //f(0);
-        System.out.println(fib(7));
-       // System.out.println(fact(5));
+        //System.out.println(fib(7));
 
+        System.out.println(fact(5));
 
 
     }
 
 
-    public static void f(int arg){
+    public static void f(int arg) {
         System.out.print(" " + arg);
-        if (arg < 10){
+        if (arg < 10) {
             f(arg + 1);
         }
         System.out.print(" -" + arg);
     }
 
 
+    public static int fib(int n) {
 
-    public static int fib (int n){
+        if (n == 0) return 1;
+        if (n == 1) return 1;
 
-        if(n == 0) return 1;
-        if(n == 1) return 1;
-
-        return fib(n - 1) + fib (n - 2);
+        return fib(n - 1) + fib(n - 2);
 
     }
-//  5! = 1*2*3*4*5
-    public static int fact(int n){
-        if(n == 0){
-           return 1;
+
+    //  5! = 1*2*3*4*5
+    public static int fact(int n) {
+
+        if (n > 0) {
+            res = fact(n * (n - 1));
+            n--;
         }
 
-        return fact(n * (n - 1));
+        return res;
+
     }
-
-
-
-
-
 }
